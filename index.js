08ac8be67dbdd2c68e5c0de36a5275381708e37d
OkuhleM <lindokuhlea38@gmail.com>
2021-03-30 08:52:11 +0200

    done work

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
   var myNodelist = document.getElementsByTagName("LI");
   var i;
-    for (i = 0; i < myNodelist.length; {
+    for (i = 0; i < myNodelist.length;i++) {
       var span = document.createElement("SPAN");
       var txt = document.createTextNode("\u00D7");
       span.className = "close";
@@ -12,7 +12,7 @@
     // attempting to hide the close button
     var close = document.getElementsByClassName("close");
     var i;
-    for (i = 0; i < close.length; {
+    for (i = 0; i < close.length;i++) {
     close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
@@ -46,7 +46,7 @@
       span.appendChild(txt);
       li.appendChild(span);
 
-      for (i = 0; i < close.length; {
+      for (i = 0; i < close.length;i++) {
     close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
